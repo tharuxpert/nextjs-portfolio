93c4a97c659afe15a7ee705b782725b3168971f4
tharusha-nax <tharusha.nax@gmail.com>
2024-08-05 23:41:27 +0530

    trying to fix the error...

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 // components
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 

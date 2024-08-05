3aacbe0102b9e60c82c28f12fd37f60f39033e30
tharusha-nax <tharusha.nax@gmail.com>
2024-08-05 20:02:24 +0530

    stairs animation

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,11 +3,13 @@ import "./globals.css";
 
 // components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: '--font-jetbrainsMono'
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
@@ -19,8 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
-        <Header/>
-        {children}</body>
+        <Header />
+        <StairTransition/>
+        <PageTransition>{children}</PageTransition>
+      </body>
     </html>
   );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -27,10 +27,22 @@ const links = [
 ];
 
 const Nav = () => {
+  const pathname = usePathname();
+  // console.log(pathname);
   return (
-    <nav className='flex gap-8'>
+    <nav className="flex gap-8">
       {links.map((link, index) => {
-        return <Link href={link.path} key={index} className=''>{link.name}</Link>;
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
       })}
     </nav>
   );

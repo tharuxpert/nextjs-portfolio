2595bf4164d4ef111a2959ab51ba4c6f82b5e00a
tharusha-nax <tharusha.nax@gmail.com>
2024-08-07 23:36:24 +0530

    done homepage

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -14,7 +15,8 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Tharusha Nirmal</span>
+              Hello I'm <br />{" "}
+              <span className="text-accent">Tharusha Nirmal</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences and I am
@@ -31,7 +33,10 @@ const Home = () => {
                 <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
@@ -41,6 +46,8 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <Stats />
     </section>
   );
 };
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,50 @@
+'use client'
+
+import { BsArrowDownRight } from 'react-icons/bs'
+import Link from 'next/link';
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. In architecto.",
+    href: ''
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. In architecto.",
+    href: ''
+  },
+  {
+    num: "03",
+    title: "Logo Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. In architecto.",
+    href: ''
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. In architecto.",
+    href: ''
+  },
+];
+
+import { motion } from 'framer-motion';
+
+
 const Services = () => {
-  return <div>Services page</div>;
+  return <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+    <div className="container max-auto">
+      <motion.div>
+        
+      </motion.div>
+    </div>
+  </section>;
 }
 
 export default Services;

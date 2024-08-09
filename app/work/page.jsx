16526300ba5a71408bdbabac869e1f5e0bd8694e
tharusha-nax <tharusha.nax@gmail.com>
2024-08-09 21:50:10 +0530

    work page...

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,83 @@
-const Work = () => {
-  return <div>Work page</div>;
-}
+"use client";
 
-export default Work
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, sunt saepe.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, sunt saepe.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, sunt saepe.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
+    image: "/assests/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
+
+const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{ project.num}</div>
+            </div>
+            {/* project category */}
+            <h2>{ project.category} project</h2>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Work;

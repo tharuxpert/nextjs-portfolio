aa750e89db7a49a070099c5fa7df51ffe98f0dfd
tharusha-nax <tharusha.nax@gmail.com>
2024-08-12 23:26:40 +0530

    finalized the project

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+"use client";
 
-const WorkSliderBtns = () => {
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkSliderBtns = ({ containerStyles, btnStyles, iconStyles }) => {
+  const swiper = useSwiper();
+
   return (
-    <div>
-      
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={iconStyles} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={iconStyles} />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default WorkSliderBtns
+export default WorkSliderBtns;
